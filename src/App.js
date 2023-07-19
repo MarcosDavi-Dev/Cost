f37fd2d202743c5c980ddeec64e49dc5b098071c
lastergates <adamsdavi77@gmail.com>
2023-07-19 06:52:38 -0300

    Colocando as páginas de navegação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home.Js';
+import Company from './components/pages/Company.JS';
+import Contact from './components/pages/Contact.JS';
+import NewProject from './components/pages/Newproject';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Routes>
+        <Route exact path='/' element={<Home/>}>
+          <Home />
+        </Route>
+        <Route exact path='/company'element={<Company/>}>
+          <Company />
+        </Route>
+        <Route exact path='/contact'element={<Contact/>}>
+          <Contact />
+        </Route>
+        <Route exact path='/newproject'element={<NewProject/>}>
+          <NewProject />
+        </Route>
+        </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
