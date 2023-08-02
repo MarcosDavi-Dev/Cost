a6c7b89805e2af194fa55dddb83061326f6cddf9
lastergates <adamsdavi77@gmail.com>
2023-08-02 06:15:18 -0300

    Integração da API com o DB feita

--- src/components/pages/Newproject.js
+++ src/components/pages/Newproject.js
@@ -1,12 +1,40 @@
+import { useNavigate } from 'react-router-dom'
+
 import styles from './Newproject.module.css'
 import ProjectForm from '../project/ProjectForm'
 
 function NewProject() {
+
+    const history = useNavigate()
+
+    function createPost (project) {
+
+        //inicialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(project),
+            }) .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                //redirect
+                history('/projects', {message: 'Projeto criado com sucesso!'})
+            })
+            .catch((err) => console.log(err))
+
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
         </div>
     )
 }
