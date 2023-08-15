390f97d78af721256299d144af2509d06aa9259a
lastergates <adamsdavi77@gmail.com>
2023-08-14 21:36:43 -0300

    Página de projetos com CSS concluída

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import styles from './ProjectCard.module.css'
 
 import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
@@ -10,11 +11,15 @@ function ProjectCard ({id, name, budget, category, handleRemove}) {
                 <span>Orçamento:</span> R${budget}
             </p>
             <p className={styles.category_text}>
-                <span></span> {category}
+                <span className={`${styles[category.toLowerCase()]}`}></span> {category}
             </p>
-            <div>
-                <p>Editar</p>
-                <p>Remover</p>
+            <div className={styles.project_card_actions}>
+                <Link to="/">
+                    <BsPencil /> Editar
+                </Link>
+                <button>
+                    <BsFillTrashFill /> Excluir
+                </button>
             </div>
         </div>
     )
