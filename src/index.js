c787e1bd04379c3fda990b96f8ffbc04d74f3eba
lastergates <adamsdavi77@gmail.com>
2023-08-31 20:50:17 -0300

    configuração do firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD53QXX-eLRMHf6dD9HJVGrcplaTUosyVo",
+  authDomain: "costs-4c8eb.firebaseapp.com",
+  projectId: "costs-4c8eb",
+  storageBucket: "costs-4c8eb.appspot.com",
+  messagingSenderId: "327524349967",
+  appId: "1:327524349967:web:a20a49e0236baa949b4b7a",
+  measurementId: "G-VP01XN2T0E"
+};
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
