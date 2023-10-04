6e25cac1d8b54ffacc9299125b9138314c17001d
lastergates <adamsdavi77@gmail.com>
2023-10-04 07:06:30 -0300

    Serviços resgatados do banco

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -7,12 +7,14 @@ import {useState, useEffect} from 'react'
 import Message from '../layout/Message'
 import ProjectForm from '../project/ProjectForm'
 import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard'
 
 function Project () {
 
      const {id} = useParams ()
 
      const [project, setProject] = useState([])
+     const [services, setServices] = useState([])
      const [showProjectForm, setShowProjectForm] = useState(false)
      const [showServiceForm, setShowServiceForm] = useState(false)
      const [message, setMessage] = useState()
@@ -28,6 +30,7 @@ function Project () {
      }).then(resp => resp.json())
      .then((data) =>{
       setProject(data)
+      setServices(data.services)
      })
      .catch(err => console.log)
 
@@ -98,6 +101,8 @@ function Project () {
     .catch(err => console.log(err))
  }
 
+ function removeService () {}
+
  function toggleProjectForm () {
   setShowProjectForm(!showProjectForm) }
 
@@ -150,7 +155,19 @@ function Project () {
                  </div>
                 <h2>Serviços</h2>
                 <div className={styles.start}>
-                    <p>Itens de serviços</p>
+                    {services.length > 0 &&
+                        services.map((service) => (
+                            <ServiceCard 
+                                id={service.id}
+                                name={service.name}
+                                cost={service.cost}
+                                description={service.description}
+                                key={service.id}
+                                handleRemove={removeService}
+                            />
+                        ))
+                    }
+                    {services.length === 0 && <p>Não há serviços cadastrados.</p>}
                 </div>
              </div>
      </div>
