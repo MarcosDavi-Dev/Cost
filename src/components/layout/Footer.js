fbf99b9f17fe117c36e7df7dcc8b2d63f7f31d05
lastergates <adamsdavi77@gmail.com>
2023-10-10 21:42:56 -0300

    Conclusão do projeto

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,23 +1,31 @@
-import {FaGithub, FaInstagram, FaLinkedin} from 'react-icons/fa'
-
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
 import styles from './footer.module.css'
-function Footer () {
-    return (<footer className={styles.footer}>
-        <ul className={styles.social_list}>
-            <li>
-                <FaGithub />
-            </li>
-            <li>
-                <FaInstagram />
-            </li>
-            <li>
-                <FaLinkedin />
-            </li>
-        </ul>
-        <p className={styles.copy_right}>
-            <span>Costs</span> &copy; 2023
-        </p>
-    </footer>)
-    }
-    
-    export default Footer
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.social_list}>
+        <li>
+        <a href="https://github.com/MarcosDaviDev" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/laster_gates/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/marcos-davi-a56202212/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+        </li>
+      </ul>
+      <p className={styles.copy_right}>
+        <span>Costs</span> &copy; 2023
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
